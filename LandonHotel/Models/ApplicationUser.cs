1a76ea15261b24d11ac6d6c87343386d4a513bf1
Edwin Corea Bonilla <ecorea@cruxconsultores.com>
2018-07-18 10:14:30 -0600

    Fixing compilation errors.

---
+++ LandonHotel/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace LandonHotel.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime Birthdate { get; set; }
+    }
+}
