71d8628e37b0e7ba1e8165394a6f1cb88d54e72f
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-18 22:08:07 -0600

    Analysing legacy code before TDD. Minor refactoring.

--- LandonHotel/Services/BookingService.cs
+++ LandonHotel/Services/BookingService.cs
@@ -5,10 +5,16 @@ namespace LandonHotel.Services
 {
     public class BookingService : IBookingService
     {
+        private readonly IRoomsRepository _roomRepo;
+
+        public BookingService(IRoomsRepository roomRepo)
+        {
+            _roomRepo = roomRepo;
+        }
+
         public decimal CalculateBookingPrice(Booking booking)
         {
-            var roomRepo = new RoomsRepository();
-            var room = roomRepo.GetRoom(booking.RoomId);
+            var room = _roomRepo.GetRoom(booking.RoomId);
 
             var numberOfNights = (booking.CheckOutDate - booking.CheckInDate).Days;
             return room.Rate * numberOfNights;
