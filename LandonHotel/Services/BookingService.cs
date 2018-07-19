8b429edf8546e788bf6fd4da1455176462faef29
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-18 22:21:15 -0600

    Confidently change your code, part 1.

--- LandonHotel/Services/BookingService.cs
+++ LandonHotel/Services/BookingService.cs
@@ -6,10 +6,12 @@ namespace LandonHotel.Services
     public class BookingService : IBookingService
     {
         private readonly IRoomsRepository _roomRepo;
+        private readonly ICouponRepository _couponRepo;
 
-        public BookingService(IRoomsRepository roomRepo)
+        public BookingService(IRoomsRepository roomRepo, ICouponRepository couponRepo)
         {
             _roomRepo = roomRepo;
+            _couponRepo = couponRepo;
         }
 
         public decimal CalculateBookingPrice(Booking booking)
@@ -17,7 +19,16 @@ namespace LandonHotel.Services
             var room = _roomRepo.GetRoom(booking.RoomId);
 
             var numberOfNights = (booking.CheckOutDate - booking.CheckInDate).Days;
-            return room.Rate * numberOfNights;
+            var price = room.Rate * numberOfNights;
+
+            if (booking.CouponCode != null)
+            {
+                var discount = _couponRepo.GetCoupon(booking.CouponCode).PercentageDiscount;
+
+                price = price - (price * discount / 100);
+            }
+
+            return price;
         }
     }
 }
